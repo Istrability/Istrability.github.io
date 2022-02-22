b814752efe38aa56e286b339a11e53c0d1269ce8
croraf <vrafaeli@msn.com>
2022-02-22 01:01:24 +0100

    Add images and change langauges

--- src/views/Home/ImageSwitcher.js
+++ src/views/Home/ImageSwitcher.js
@@ -1,54 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import titleComposed1 from "../../resources/office3.jpg";
+import titleComposed2 from "../../resources/office2.jpg";
+import titleComposed3 from "../../resources/office1.jpg";
+/* import titleComposed4 from "../../resources/titleComposed1.jpg";
+import titleComposed5 from "../../resources/titleComposed2.jpg"; */
+import { makeStyles } from "@mui/styles";
 
-import titleComposed1 from '../../resources/titleComposed1.jpg';
-import titleComposed2 from '../../resources/titleComposed2.jpg';
-import { makeStyles } from '@mui/styles';
+const overlay =
+  "linear-gradient(to bottom, rgba(0, 0, 8, 1), rgba(0,0,0,0), rgba(255, 255, 255, 0.40))";
 
 const useStyles = makeStyles({
   imageContainer: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: '1rem',
-    height: '60vh',
-    textAlign: 'center',
-    width: '100%',
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "1rem",
+    height: "60vh",
+    textAlign: "center",
+    width: "100%",
     //border: '5px solid black',
-    borderLeft: '0px solid black',
-    borderRight: '0px solid black',
-    boxSizing: 'content-box',
-    position: 'relative',
-    background: 'black',
+    borderLeft: "0px solid black",
+    borderRight: "0px solid black",
+    boxSizing: "content-box",
+    position: "relative",
+    background: "black",
   },
   image: {
-    filter: 'brightness(47%)',
-    width: '100%',
-    height: '100%',
-    backgroundRepeat: 'repeat-x',
-    backgroundSize: '280vh 60vh',
-    backgroundPosition: '50%',
-    transition: 'opacity 2s ease-in-out',
-    position: 'absolute',
+    filter: "brightness(47%)",
+    width: "100%",
+    height: "100%",
+    backgroundRepeat: "repeat-x",
+    backgroundSize: "240vh 80vh",
+    backgroundPosition: "50%",
+    transition: "opacity 2s ease-in-out",
+    position: "absolute",
   },
   textContainer: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 50,
-    color: 'white',
-    fontSize: '2rem',
-    width: '100%',
-    height: '100%',
-    textAlign: 'center',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    color: "white",
+    fontSize: "2rem",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   /* '@media (min-width: 600px)': {
       
   }, */
-  '@media (min-width: 960px)': {
+  "@media (min-width: 960px)": {
     imageContainer: {
-      marginTop: '1rem',
-      marginBottom: '0.5rem',
+      marginTop: "1rem",
+      marginBottom: "0.5rem",
     },
   },
 });
@@ -59,7 +64,7 @@ const ImageSwitcher = () => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setFrontImageIndex((frontImageIndex + 1) % 2);
+      setFrontImageIndex((frontImageIndex + 1) % 3);
     }, 6500);
 
     return () => clearTimeout(timeout);
@@ -67,10 +72,41 @@ const ImageSwitcher = () => {
 
   return (
     <div className={classes.imageContainer}>
-      <div className={classes.image} style={{ backgroundImage: `url(${titleComposed1})`, backgroundColor: '#0B0B18', opacity: frontImageIndex === 0 ? 1 : 0 }} />
-      <div className={classes.image} style={{ backgroundImage: `url(${titleComposed2})`, backgroundColor: '#0B0B18', opacity: frontImageIndex === 1 ? 1 : 0 }} />
+      <div
+        className={classes.image}
+        style={{
+          backgroundImage: `${overlay}, url(${titleComposed1})`,
+          backgroundColor: "#080814",
+          opacity: frontImageIndex === 0 ? 1 : 0,
+        }}
+      />
+      <div
+        className={classes.image}
+        style={{
+          backgroundImage: `${overlay}, url(${titleComposed2})`,
+          backgroundColor: "#080814",
+          opacity: frontImageIndex === 1 ? 1 : 0,
+        }}
+      />
+      <div
+        className={classes.image}
+        style={{
+          backgroundImage: `${overlay}, url(${titleComposed3})`,
+          backgroundColor: "#080814",
+          opacity: frontImageIndex === 2 ? 1 : 0,
+        }}
+      />
       <div className={classes.textContainer}>
-        <div style={{fontSize: '3rem', fontWeight: 'bold', marginBottom: '2rem', marginTop: '-4rem'}}>Istrability</div>
+        <div
+          style={{
+            fontSize: "3rem",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+            marginTop: "-4rem",
+          }}
+        >
+          Istrability
+        </div>
         <div>Digitalize your ideas</div>
       </div>
     </div>
--- src/views/Home/TitleRow/index.tsx
+++ src/views/Home/TitleRow/index.tsx
@@ -1,77 +1,77 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import { Link } from 'react-router-dom';
-import Flags from 'country-flag-icons/react/3x2'
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import { Link } from "react-router-dom";
+import Flags from "country-flag-icons/react/3x2";
 
 /* import SpreadMenu from './SpreadMenu'; */
 /* import SidebarMenu from './SidebarMenu'; */
 
 const useStyles = makeStyles({
   titleRow: {
-    position: 'sticky',
-    top: '0rem',
-    backgroundColor: '#0B0B18',
-    width: '100%',
-    padding: '0rem 1.5rem 0rem 1.5rem',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    color: '#FFFFFE',
-    height: '3rem',
-    boxSizing: 'border-box',
+    position: "sticky",
+    top: "0rem",
+    backgroundColor: "#0B0B18",
+    width: "100%",
+    padding: "0rem 1.5rem 0rem 1.5rem",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    color: "#FFFFFE",
+    height: "3rem",
+    boxSizing: "border-box",
     zIndex: 100,
     // boxShadow: '0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%)'
   },
   titlePlaceholder: {
-    height: '1rem',
-    top: '4rem',
-    backgroundColor: '#0B0B18',
-    position: 'absolute',
+    height: "1rem",
+    top: "4rem",
+    backgroundColor: "#0B0B18",
+    position: "absolute",
     //zIndex: 1000,
-    width: '100%',
+    width: "100%",
   },
   mdMenu: {
-    display: 'none',
+    display: "none",
   },
   xsMenu: {
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
   },
   flag: {
-    width: '1.4rem', 
+    width: "1.4rem",
   },
   title: {
-    color: '#FFFFFE',
-    fontSize: '1.5rem',
+    color: "#FFFFFE",
+    fontSize: "1.5rem",
     // fontWeight: 'bold',
-    cursor: 'pointer',
-    textDecoration: 'none',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    cursor: "pointer",
+    textDecoration: "none",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
   titleIcon: {
-    fontSize: '1.2rem',
+    fontSize: "1.2rem",
   },
   /* '@media (min-width: 600px)': {
       
   }, */
-  '@media (min-width: 960px)': {
+  "@media (min-width: 960px)": {
     titleRow: {
-      height: '4rem',
-      paddingRight: '3.5rem',
+      height: "4rem",
+      paddingRight: "3.5rem",
     },
     titlePlaceholder: {
-      top: '5rem',
+      top: "5rem",
     },
     title: {
-      fontSize: '1.8rem',
+      fontSize: "1.8rem",
     },
     flag: {
-      width: '1.5rem',
-      cursor: 'pointer',
+      width: "1.5rem",
+      cursor: "pointer",
     },
     mdMenu: {
       //display: 'flex',
@@ -82,60 +82,64 @@ const useStyles = makeStyles({
         display: 'none',
     }, */
     titleIcon: {
-      fontSize: '1.8rem',
+      fontSize: "1.8rem",
     },
   },
 });
 
+const TitleRow =
+  (/* { servicesClickHandler, contactClickHandler, logoClickedHandler, currentRoute } */) => {
+    const classes = useStyles();
+    const [language, setLanguage] = useState("US");
 
-const TitleRow = (/* { servicesClickHandler, contactClickHandler, logoClickedHandler, currentRoute } */) => {
-  const classes = useStyles();
-  const [language, setLanguage] = useState('US');
+    return (
+      <>
+        <div
+          style={{ height: "1rem", backgroundColor: "#0B0B18", width: "100%" }}
+        />
+        <div className={classes.titlePlaceholder} />
+        <div className={classes.titleRow}>
+          <Link to="/" className={classes.title}>
+            Istrability
+          </Link>
 
-  return (
-    <>
-      <div style={{ height: '1rem', backgroundColor: '#0B0B18', width: '100%' }} />
-      <div className={classes.titlePlaceholder} />
-      <div className={classes.titleRow}>
-        <Link
-          to='/'
-          className={classes.title}
-        >
-          Istrability
-        </Link>
-
-        {/* <div className={classes.mdMenu}>
+          {/* <div className={classes.mdMenu}>
                     <SpreadMenu />
                 </div> */}
 
-        <div className={classes.xsMenu}>
-          <div
-            className={classes.flag}
-            style={{marginRight: '1rem', opacity: language === 'US' ? 1 : 0.5}}
-            onClick={() => setLanguage('US')}
-          >
-            <Flags.US title={'English'} />
+          <div className={classes.xsMenu}>
+            <div
+              className={classes.flag}
+              style={{
+                marginRight: "1rem",
+                opacity: language === "US" ? 1 : 0.5,
+              }}
+              onClick={() => setLanguage("US")}
+            >
+              <Flags.US title={"English"} />
+            </div>
+            <div
+              className={classes.flag}
+              style={{
+                marginRight: "1rem",
+                opacity: language === "HR" ? 1 : 0.5,
+              }}
+              onClick={() => setLanguage("HR")}
+            >
+              <Flags.HR title={"Hrvatski"} />
+            </div>
+            <div
+              className={classes.flag}
+              style={{ opacity: language === "IT" ? 1 : 0.5 }}
+              onClick={() => setLanguage("IT")}
+            >
+              <Flags.IT title={"Italiano"} />
+            </div>
+            {/* <SidebarMenu /> */}
           </div>
-          <div
-            className={classes.flag}
-            style={{marginRight: '1rem', opacity: language === 'HR' ? 1 : 0.5}}
-            onClick={() => setLanguage('HR')}
-          >
-            <Flags.HR title={'Hrvatski'} />
-          </div>
-          <div
-            className={classes.flag}
-            style={{opacity: language === 'FR' ? 1 : 0.5}}
-            onClick={() => setLanguage('FR')}
-          >
-            <Flags.FR title={'Français'} />
-          </div>
-          {/* <SidebarMenu /> */}
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  };
 
 export default TitleRow;
-
