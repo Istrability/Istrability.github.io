21f1760e86e59e8a65884fcbc79e4dec59b3b6a1
croraf <vrafaeli@msn.com>
2023-01-05 08:42:57 +0100

    Add italian translation

--- src/translations/it.ts
+++ src/translations/it.ts
@@ -1,12 +1,12 @@
 const it = {
-  title: "Digitalize your ideas",
-  aboutUs: "About us",
+  title: "Digitalizza le tue idee",
+  aboutUs: "Chi siamo",
   aboutUsContent1:
-    "Istrability is a software development company that is actively developing Web apps (and other software) since 2019. With the help of our expert knowledge you can realize your digital ideas or optimize your business processes.",
+    "Istrability è un'azienda di sviluppo software che sviluppa attivamente le applicazioni web (e altro software) dal 2019. Grazie alla nostra esperta conoscenza, puoi realizzare le tue idee digitali o ottimizzare i tuoi processi aziendali.",
   aboutUsContent2:
-    "Our area of expertize is building frontend part of Web apps (in React), backend part of Web apps and scripting (in NodeJS), whole Web apps, and system apps (in Rust).",
-  contactAndLocation: "Contact & Location",
-  productsAndServices: "Products & Services",
+    "Il nostro campo di espertise è la costruzione della parte frontend delle applicazioni web (in React), della parte backend delle applicazioni web e dello scripting (in NodeJS), delle applicazioni web complete e delle applicazioni di sistema (in Rust).",
+  contactAndLocation: "Contatto & Posizione",
+  productsAndServices: "Prodotti & Servizi",
 };
 
 export default it;
