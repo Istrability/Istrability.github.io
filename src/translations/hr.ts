3ef44d09e7de6c9cf14e4dab21cbabd4254ab0a9
croraf <vrafaeli@msn.com>
2025-03-30 10:44:38 +0200

    minor typo and wording fixes

--- src/translations/hr.ts
+++ src/translations/hr.ts
@@ -2,10 +2,10 @@ const hr = {
   title: "Digitaliziraj svoje ideje",
   aboutUs: "O nama",
   aboutUsContent1:
-    "Istrability je softverska kompanija koja samostalno izrađuje Web aplikacije (i drugi softver) od 2019. godine. Uz našu profesionalnu pomoć možete realizirati i digitalizirati vaše ideje te optimizirati svoje poslovne procese.",
+    "Istrability je softverska kompanija koja samostalno izrađuje Web aplikacije (i drugi softver) od 2019. godine. Uz našu profesionalnu pomoć možete realizirati vaše informatičke ideje te optimizirati svoje poslovne procese.",
   aboutUsContent2:
     "Specijalizirani smo za izradu gotovih Web programskih rješenja (u React-u i NodeJS-u), te gotovih sistemskih aplikacija.",
-  contactAndLocation: "Kontak & Lokacija",
+  contactAndLocation: "Kontakt & Lokacija",
   productsAndServices: "Proizvodi & Usluge",
 };
 
